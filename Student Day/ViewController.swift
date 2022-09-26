5893a5bd63ee76801fd88fc5a81f635f6eac176a
SHREDDING <zavrazhnov.zhora@mail.ru>
2022-09-26 12:37:07 +0300

    Initial Commit

---
+++ Student Day/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Student Day
+//
+//  Created by SHREDDING on 26.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
