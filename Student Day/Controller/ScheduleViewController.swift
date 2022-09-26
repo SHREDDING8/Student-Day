1760329665a4a085af720ed83b6bc94f86fad4e8
SHREDDING <zavrazhnov.zhora@mail.ru>
2022-09-27 00:56:38 +0300

    Added the background color for light and dark theme added calendar added button to show and close -> realize logic when data is selected the calendar will be full

---
+++ Student Day/Controller/ScheduleViewController.swift
@@ -0,0 +1,150 @@
+//
+//  ScheduleViewController.swift
+//  Student Day
+//
+//  Created by SHREDDING on 26.09.2022.
+//
+
+
+/*
+ TO DO: -
+  - Сделать плавный переход при неизменении страницы с помощью просмотра даты в начале развертывания
+ 
+  - Изменить цвета и вид календаря на приятный не забыть темную тему
+
+  - подумать на хедером
+ */
+
+import UIKit
+import FSCalendar
+
+class ScheduleViewController: UIViewController {
+    
+    // MARK: -  Objects
+    
+    private var calendarView:FSCalendar = {
+        let calendar = FSCalendar()
+        calendar.translatesAutoresizingMaskIntoConstraints = false
+        calendar.firstWeekday = 2
+        calendar.scope = .week
+        calendar.appearance.headerMinimumDissolvedAlpha = 0
+        calendar.locale = NSLocale(localeIdentifier: "ru") as Locale
+        calendar.appearance.headerDateFormat = "MM"
+        return calendar
+    }()
+    var calendarHeightConstraint:NSLayoutConstraint!
+    
+    private var showCloseCalendarButton:UIButton = {
+        let button = UIButton()
+        button.setTitle("Развернуть", for: .normal)
+        button.setTitleColor(UIColor.orange, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setView()
+        setNavigationBar()
+        setCalendar()
+        setButtonShowCloseCalendar()
+
+    }
+    
+
+// MARK: - SET VIEW
+    
+    private func setView(){
+        self.view.backgroundColor = UIColor(named: "background Color")
+    }
+    
+    
+//MARK: - navigation Bar
+    private func setNavigationBar(){
+        let editNavItem = UIBarButtonItem(barButtonSystemItem: .edit, target: nil, action: nil)
+        self.navigationItem.rightBarButtonItem = editNavItem
+        
+    }
+
+//MARK: - CALENDAR
+    
+    private func setCalendar(){
+        
+        self.view.addSubview(calendarView)
+        
+         calendarHeightConstraint = NSLayoutConstraint(item: calendarView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 300)
+        calendarView.addConstraint(calendarHeightConstraint)
+        
+        NSLayoutConstraint.activate([
+            calendarView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            calendarView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0),
+            calendarView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 0)]
+        )
+        calendarView.delegate = self
+        calendarView.dataSource = self
+    }
+    
+//MARK: - Button
+    private func setButtonShowCloseCalendar(){
+        
+        self.view.addSubview(showCloseCalendarButton)
+        
+        
+        NSLayoutConstraint.activate([
+            showCloseCalendarButton.topAnchor.constraint(equalTo: calendarView.bottomAnchor, constant: 10),
+            showCloseCalendarButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20),
+            showCloseCalendarButton.heightAnchor.constraint(equalToConstant: 20)
+        ])
+        
+        showCloseCalendarButton.addTarget(nil, action: #selector(buttonShowCloseTapped), for: .touchUpInside)
+    }
+    
+    @objc private func buttonShowCloseTapped(){
+        if calendarView.scope == .week{
+            // change the scope
+            calendarView.setScope(.month, animated: true)
+            
+            // change the text of button
+            UIView.transition(with: showCloseCalendarButton , duration: 0.5, options: .transitionFlipFromTop, animations: {
+                self.showCloseCalendarButton.setTitle( "Скрыть", for: .normal)
+            }, completion: nil)
+        }else{
+            
+            
+            if let today =  calendarView.today{
+                if calendarView.currentPage == today{
+                    calendarView.setScope(.week, animated: true)
+                }
+                else{
+                    print(calendarView.currentPage)
+                    calendarView.setScope(.week, animated: false)
+                    calendarView.setCurrentPage(today, animated: true)
+                }
+               
+            }
+            UIView.transition(with: showCloseCalendarButton , duration: 0.5, options: .transitionFlipFromTop, animations: {
+                self.showCloseCalendarButton.setTitle( "Развернуть", for: .normal)
+            }, completion: nil)
+            if let selected = calendarView.selectedDate{
+                calendarView.deselect(selected)
+            }
+            
+        }
+    }
+    
+}
+
+//MARK: - CALENDAR FSCalendarDelegate,FSCalendarDataSource
+extension ScheduleViewController:FSCalendarDelegate,FSCalendarDataSource{
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+            self.calendarView.setScope(.month, animated: true)
+        
+    }
+    func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
+        calendarHeightConstraint.constant = bounds.height
+        view.layoutIfNeeded()
+    }
+}
